af2c570f90ac1305ba39ad030898689b88151667
bengfarrell <ben@benfarrell.com>
2018-10-04 22:29:11 -0700

    added step2

---
+++ chapter9/9.3-demo/step2/components/workoutcreatorapp/template.js
@@ -0,0 +1,37 @@
+import ExerciseLibrary from '../exerciselibrary/exerciselibrary.js';
+import Plan from '../plan/plan.js';
+
+export default {
+    render() {
+        return `${this.css()}
+                ${this.html()}`;
+    },
+
+    html() {
+        return `<wkout-exercise-lib></wkout-exercise-lib>
+                <div id="divider-line"></div>
+                <wkout-plan></wkout-plan>`;
+    },
+
+    css() {
+        return `<style>
+                    :host {
+                       display: flex;
+                    }
+                    
+                    wkout-exercise-lib,
+                    wkout-plan {
+                        flex: 1;
+                        height: 100%;
+                        background-color: #eaeaea;
+                    }
+                    
+                    #divider-line {
+                        width: 1px;
+                        height: 100%;
+                        margin-right: 25px;
+                        background-color: black;
+                    }
+                </style>`;
+    }
+}
