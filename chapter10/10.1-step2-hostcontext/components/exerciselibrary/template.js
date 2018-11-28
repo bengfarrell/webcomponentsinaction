758b5b4eaf84fed6b936c7b655d5a486d9c136dc
bengfarrell <ben@benfarrell.com>
2018-11-27 23:13:54 -0800

    minor fixes according to feedback

--- chapter10/10.1-step2-hostcontext/components/exerciselibrary/template.js
+++ chapter10/10.1-step2-hostcontext/components/exerciselibrary/template.js
@@ -11,7 +11,7 @@ export default {
         let mkup = `<h1>Exercises</h1>
                     <div id="container">`;
         for (let c = 0; c < exercises.length; c++) {
-            mkup += `<wkout-exercise class="${exercises[c].type}" ${Exercise.toAttributeString(exercises[c])}" ></wkout-exercise>`;
+            mkup += `<wkout-exercise class="${exercises[c].type}" ${Exercise.toAttributeString(exercises[c])}></wkout-exercise>`;
         }
         return mkup + `</div>`;
     },
