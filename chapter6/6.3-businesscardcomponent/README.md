c67bac5d3d01088e2de4b6ce8f5dd126d4887a50
bengfarrell <ben@benfarrell.com>
2018-12-01 16:17:55 -0800

    cleaned up template formatting to make it not look like it came from the node tool. Also put instructions on node tool in readme. I don't get into it in detail beyond a mention in the book anymore....too much a distraction.

---
+++ chapter6/6.3-businesscardcomponent/README.md
@@ -0,0 +1,14 @@
+Automated Tool to inject contents of an HTML file into a JS module for use as a template
+-----
+
+(example is already complete without running tool, so feel free to ignore)
+
+1. cd to 6.3-businesscardcomponent directory
+2. Run node command on the script at bin/html2template.js
+    a. Param one is the source HTML file
+    b. Param two is the destination JS module
+
+To run in the context of this example, run the following in the command prompt from this directory:
+
+node bin/html2template.js htmlsource/bizcard.html components/bizcard/template.js
+
--- chapter6/6.3-businesscardcomponent/components/bizcard/template.js
+++ chapter6/6.3-businesscardcomponent/components/bizcard/template.js
@@ -4,63 +4,66 @@ export default {
                 ${this.css(props)}`;
     },
 
-    html(p) { return ` <div class="biz-card">
-    <div class="logo"></div>
-    <div class="top-text">
-        <h1>${p.first_name} ${p.last_name}</h1>
-        <h3>${p.title}</h3>
-    </div>
+    html(p) {
+        return ` <div class="biz-card">
+                    <div class="logo"></div>
+                    <div class="top-text">
+                        <h1>${p.first_name} ${p.last_name}</h1>
+                        <h3>${p.title}</h3>
+                    </div>
+                
+                    <div class="bottom-text">
+                        <h3>phone: ${p.phone}</h3>
+                        <h3>${p.email} / ${p.website}</h3>
+                    </div>
+                </div>`;
+        },
 
-    <div class="bottom-text">
-        <h3>phone: ${p.phone}</h3>
-        <h3>${p.email} / ${p.website}</h3>
-    </div>
-</div>`; },
-
-    css(p) { return ` <style>
-    .biz-card {
-        font-size: 16px;
-        font-family: sans-serif;
-        color: white;
-        width: 700px;
-        height: 400px;
-        display: inline-block;
-        border-color: #9a9a9a;
-        background-size: 5%;
-        background-image: url("background-pattern.png");
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    }
-
-    .biz-card .logo {
-        height: 100px;
-        margin-top: 10%;
-        text-align: center;
-        background-image: url("biz-card-logo.png");
-        background-size: contain;
-        background-position-x: center;
-        background-repeat: no-repeat;
-    }
-
-    .biz-card .top-text {
-        text-align: center;
-    }
-
-    .biz-card .top-text h1 {
-        font-size: 2.5em;
-        margin-bottom: 0;
-    }
-
-    .biz-card .top-text h3 {
-        margin: 0;
-    }
-
-    .biz-card .bottom-text {
-        text-align: center;
-        margin-top: 10%;
-    }
-
-    .biz-card .bottom-text h3 {
-        margin: 0;
-    }
-</style>`; },
+    css(p) { return `<style>
+                        .biz-card {
+                            font-size: 16px;
+                            font-family: sans-serif;
+                            color: white;
+                            width: 700px;
+                            height: 400px;
+                            display: inline-block;
+                            border-color: #9a9a9a;
+                            background-size: 5%;
+                            background-image: url("background-pattern.png");
+                            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                        }
+                    
+                        .biz-card .logo {
+                            height: 100px;
+                            margin-top: 10%;
+                            text-align: center;
+                            background-image: url("biz-card-logo.png");
+                            background-size: contain;
+                            background-position-x: center;
+                            background-repeat: no-repeat;
+                        }
+                    
+                        .biz-card .top-text {
+                            text-align: center;
+                        }
+                    
+                        .biz-card .top-text h1 {
+                            font-size: 2.5em;
+                            margin-bottom: 0;
+                        }
+                    
+                        .biz-card .top-text h3 {
+                            margin: 0;
+                        }
+                    
+                        .biz-card .bottom-text {
+                            text-align: center;
+                            margin-top: 10%;
+                        }
+                    
+                        .biz-card .bottom-text h3 {
+                            margin: 0;
+                        }
+                    </style>`;
+    },
 }
